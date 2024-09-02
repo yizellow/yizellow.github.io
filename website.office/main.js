88123458e8b482ba6a45514444b597d6d998a34f
Yi-Ze Huang <158265847+yizellow@users.noreply.github.com>
2024-09-02 11:51:31 +0800

    official1

---
+++ website.office/main.js
@@ -0,0 +1,12 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("mySidebar");
+    const content = document.querySelector(".content");
+
+    if (sidebar.style.left === "-250px") {
+        sidebar.style.left = "0";
+        content.style.marginLeft = "250px";
+    } else {
+        sidebar.style.left = "-250px";
+        content.style.marginLeft = "0";
+    }
+}
