0de7863941ac07045a90ab3bf3b30a234792b38b
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-11-06 00:01:27 +0800

    tag modeling

---
+++ yizevue/src/components/infoTag.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from "vue";
+import { Icon } from "@iconify/vue";
+
+const showInfo = ref(false);
+const canceledInfo = () => {
+  showInfoTag.value = true;
+  showInfo.value = false;
+};
+const showInfoTag = ref(true);
+const openInfo = () => {
+  showInfoTag.value = false;
+  showInfo.value = true;
+};
+</script>
+<template>
+  <!--          content          -->
+
+  <div
+    v-if="showInfo"
+    class="rounded backdrop-blur-sm absolute top-1/2 right-1 -translate-y-1/2 -translate-x-1/4 w-1/4 h-auto bg-green-400/60 p-4 z-10"
+  >
+    <Icon
+      icon="material-symbols:close-rounded"
+      width="23"
+      class="cursor-pointer justify-self-end m-0 hover:scale-150 transition-transform duration-300"
+      @click="canceledInfo"
+    />
+
+    <h1 class="poppins-medium text-gray-700 text-center text-base">
+      #Plastic wrap protection<br />Laocoon and his sons
+    </h1>
+    <p class="markazi-text text-purple-900 my-3 text-2xl text-left ml-3">
+      Ukraine’s Commander-in-Chief Oleksandr Syrskyi said the situation on the
+      front line “remains difficult” and certain areas “require constant renewal
+      of resources of Ukrainian units” in a statement on Telegram Saturday
+      morning.
+    </p>
+  </div>
+  <!--            tags              -->
+
+  <div
+    id="tag"
+    class="p-3 flex row items-center fixed bottom-20 right-5 -translate-y-1/2 z-10 hover:animate-bounce cursor-pointer"
+    v-if="showInfoTag"
+    @click="openInfo"
+  >
+    <Icon
+      icon="material-symbols:arrow-circle-left-rounded"
+      width="23"
+      color="white"
+      class="mr-2 opacity-70 cursor-pointer"
+    />
+    <div class="text-green-400 opacity-80 text-2xl">Info</div>
+  </div>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Geo:ital@0;1&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Markazi+Text:wght@400..700&display=swap");
+
+.poppins-medium {
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+}
+.markazi-text {
+  font-family: "Markazi Text", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
--- yizevue/src/views/ThreeDModelView.vue
+++ yizevue/src/views/ThreeDModelView.vue
@@ -1,54 +1,32 @@
 <script setup>
 import { ref } from "vue";
 import { Icon } from "@iconify/vue";
+import infoTag from "@/components/infoTag.vue";
 
 const showIntro = ref(true);
 const canceledIntro = () => {
   showIntro.value = false;
+  showIntroTag.value = true;
+};
+const showIntroTag = ref(false);
+const openIntro = () => {
+  showIntroTag.value = false;
+  showIntro.value = true;
 };
 </script>
 
 <template>
-  <!-- <div
-    class="text-2xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/4 z-12"
-  >
-    INTRO
-  </div> -->
   <div class="-mt-14">
     <section id="1" class="relative">
-      <div
-        v-if="showIntro"
-        class="backdrop-blur-sm fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/4 h-auto bg-white/60 p-8 z-10"
-      >
-        <Icon
-          icon="material-symbols:close-rounded"
-          width="23"
-          class="justify-self-end m-0 hover:scale-150 transition-transform duration-300"
-          id="canceled"
-          @click="canceledIntro"
-        />
-
-        <h1 class="poppins-medium text-gray-800 text-center text-2xl">
-          3D Modeling, Experiment, Material and Light
-        </h1>
-        <p class="markazi-text text-gray-900 my-5 text-2xl">
-          Russia has steadily been making gains in the eastern Donbas region,
-          which Russia’s President Vladimir Putin aims to capture in full.
-          Reuters, citing open source data, reported that Russia has been
-          advancing at its fastest pace in at least a year. Ukraine’s
-          Commander-in-Chief Oleksandr Syrskyi said the situation on the front
-          line “remains difficult” and certain areas “require constant renewal
-          of resources of Ukrainian units” in a statement on Telegram Saturday
-          morning.
-        </p>
-      </div>
       <img
-        class="w-full h-full object-cover"
+        class="w-full h-full object-cover cursor-pointer"
         src="https://cdnb.artstation.com/p/assets/images/images/052/116/789/large/yize-huang-plastic-wrap-protection-laocoon-and-his-sons.jpg?1658985491"
       />
+
+      <infoTag />
     </section>
     <section
-      id="parallax-section"
+      id="2"
       class="pr-[100px] relative w-full h-[600px] overflow-hidden flex items-center justify-start bg-right bg-[#e5e4e1]"
       style="
         background-image: url('https://cdna.artstation.com/p/assets/images/images/052/282/404/large/yize-huang-sde.jpg?1659416576');
@@ -59,12 +37,13 @@ const canceledIntro = () => {
     >
       <img
         src="https://cdnb.artstation.com/p/assets/images/images/052/282/401/large/yize-huang-ft.jpg?1659416553"
-        class="w-1/4 m-10 drop-shadow-md opacity-90 hover:opacity-50"
+        class="cursor-pointer w-1/4 m-10 drop-shadow-md opacity-90 hover:opacity-50"
       />
       <img
         src="https://cdna.artstation.com/p/assets/images/images/052/282/402/large/yize-huang-new-whole.jpg?1659416565"
-        class="w-1/4 drop-shadow-md opacity-90 hover:opacity-50 hover:scale-110 transition-transform duration-300"
+        class="cursor-pointer w-1/4 drop-shadow-md opacity-90 hover:opacity-50 hover:scale-110 transition-transform duration-300"
       />
+      <infoTag />
     </section>
     <section
       id="3"
@@ -85,14 +64,60 @@ const canceledIntro = () => {
       id="4"
       class="flex justify-center items-center w-full h-screen bg-gradient-to-b from-[#010e68] to-[#4b14ff]"
     >
-      <img src="@/assets/img/3Droom.png" class="h-4/5 drop-shadow-2xl" />
+      <img
+        src="@/assets/img/3Droom.png"
+        class="cursor-pointer h-4/5 drop-shadow-2xl"
+      />
     </section>
+  </div>
+
+  <!--         tags              -->
+  <!--         tags              -->
+  <!--         tags              -->
+  <!--         tags              -->
+  <div
+    v-if="showIntro"
+    class="backdrop-blur-sm fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/4 h-auto bg-white/60 p-8 z-10"
+  >
+    <Icon
+      icon="material-symbols:close-rounded"
+      width="23"
+      class="cursor-pointer justify-self-end m-0 hover:scale-150 transition-transform duration-300"
+      id="canceled"
+      @click="canceledIntro"
+    />
+
+    <h1 class="poppins-medium text-gray-800 text-center text-2xl">
+      3D Modeling, Experiment, Material and Light
+    </h1>
+    <p class="markazi-text text-gray-900 my-5 text-2xl">
+      Russia has steadily been making gains in the eastern Donbas region, which
+      Russia’s President Vladimir Putin aims to capture in full. Reuters, citing
+      open source data, reported that Russia has been advancing at its fastest
+      pace in at least a year. Ukraine’s Commander-in-Chief Oleksandr Syrskyi
+      said the situation on the front line “remains difficult” and certain areas
+      “require constant renewal of resources of Ukrainian units” in a statement
+      on Telegram Saturday morning.
+    </p>
+  </div>
+  <div
+    v-if="showIntroTag"
+    id="introTag"
+    class="p-3 flex row items-center fixed top-1/4 right-5 -translate-y-1/2 z-10 hover:animate-bounce cursor-pointer"
+    @click="openIntro"
+  >
+    <Icon
+      icon="bitcoin-icons:plus-outline"
+      width="13"
+      color="white"
+      class="mr-2 backdrop-invert opacity-70 cursor-pointer"
+    />
+    <div class="text-white opacity-70 text-xl">INTRO</div>
   </div>
 </template>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Geo:ital@0;1&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&display=swap");
-
 @import url("https://fonts.googleapis.com/css2?family=Markazi+Text:wght@400..700&display=swap");
 
 .poppins-medium {
